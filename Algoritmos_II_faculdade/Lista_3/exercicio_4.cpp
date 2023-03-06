9068c3f13cbab4a81fefb574ca05f1ad40449153
GregoryVinicius <gregoryvinciuscla@gmail.com>
2023-03-06 15:57:53 -0300

    Lista de exercicios

---
+++ Algoritmos_II_faculdade/Lista_3/exercicio_4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+/*
+4. Escreva um programa em C que imprima os dez primeiros números inteiros maiores que 100.
+*/
+
+int main()
+{
+	for (int i = 101; i <= 110; i++)
+		printf ("\n %d",i);
+	return 0;
+}
